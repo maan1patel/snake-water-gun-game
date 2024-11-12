564eb65224b7b5532cfb0258f664e808a97312b4
Maan <101073449+maan1patel@users.noreply.github.com>
2024-11-12 17:50:49 -0500

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,45 @@
+# Snake, Water, Gun game
+import random
+
+def gameWin(comp,you):
+    if(comp == you):
+        return None
+    elif comp == "s":
+        if you == "w":
+            return False
+        elif you == "g":
+            return True
+    elif comp == "w":
+        if you == "g":
+            return False
+        elif you == "s":
+            return True
+    elif comp == "g":
+        if you == "s":
+            return False
+        elif you == "w":
+            return True
+
+print("Computer's turn: snake(s), water(w), gun(g)")
+randNo = random.randint(1,3)
+if randNo == 1:
+    comp = "s"
+elif randNo == 2:
+    comp = "w"
+elif randNo == 3:
+    comp = "g"
+
+    
+you = input("Your turn: snake(s), water(w), gun(g)")
+a = gameWin(comp,you)
+
+print(f"comp choose {comp}")
+print(f"you choose {you}")
+
+if a == None:
+    print("Match is a tie")
+elif a == True:
+    print("you won")
+else:
+    print("you loose")
+
